c2bd13ad9fa3373df7da7917b769932798015216
23MH1A05F4 <meghanacheppulla@gmail.com>
2026-02-28 18:26:38 +0530

    firstcommit

---
+++ src/main/java/com/example/demo/CsedeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsedeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsedeployApplication.class, args);
+	}
+
+}
